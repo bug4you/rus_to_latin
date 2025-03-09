f4389161b0e4b69e826bb5af7899e04690f117db
algoexpert <brogrammer.devs@gmail.com>
2025-03-09 20:56:48 +0500

    Add initial project files including README, LICENSE, .gitignore, and package.json

---
+++ pkg/rus_to_latin.js
@@ -0,0 +1,5 @@
+import * as wasm from "./rus_to_latin_bg.wasm";
+export * from "./rus_to_latin_bg.js";
+import { __wbg_set_wasm } from "./rus_to_latin_bg.js";
+__wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
---
+++ pkg/rus_to_latin_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const russian_to_latin: (a: number, b: number) => [number, number];
+export const latin_to_russian: (a: number, b: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
